39fb570129d9a95a9294ff464374148dcd2bbf83
hikaru-webcamp <skyvader0524@gmail.com>
2021-05-05 10:05:34 +0000

    2nd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  end
-end
-
-def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: [
     :email,
     :name,
@@ -26,4 +21,6 @@ def configure_permitted_parameters
     :address_street,
     :address_building
   ])
+  end
 end
+
